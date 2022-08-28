0839341c29397bb3b6e1efcef48ccb9a181f4ba9
Lakshy-1 <lvikramsah@gmail.com>
2022-08-28 13:51:55 +0545

    tips 2

---
+++ lib/tips2/chatController.dart
@@ -0,0 +1,126 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+
+class ChatProvider extends GetxController {
+  // final SharedPreferences prefs;
+  final FirebaseFirestore firebaseFirestore;
+  final FirebaseStorage firebaseStorage;
+
+  ChatProvider(
+      {required this.firebaseFirestore,
+      //required this.prefs,
+      required this.firebaseStorage});
+
+  // String? getPref(String key) {
+  //   return prefs.getString(key);
+  // }
+
+  UploadTask uploadFile(File image, String fileName) {
+    Reference reference = firebaseStorage.ref().child(fileName);
+    UploadTask uploadTask = reference.putFile(image);
+    return uploadTask;
+  }
+
+  UploadTask uploadAudio(var audioFile, String fileName) {
+    Reference reference = firebaseStorage.ref().child(fileName);
+    UploadTask uploadTask = reference.putFile(audioFile);
+    return uploadTask;
+  }
+
+  Future<void> updateDataFirestore(String collectionPath, String docPath,
+      Map<String, dynamic> dataNeedUpdate) {
+    return firebaseFirestore
+        .collection(collectionPath)
+        .doc(docPath)
+        .update(dataNeedUpdate);
+  }
+
+  Stream<QuerySnapshot> getChatStream(String groupChatId, int limit) {
+    return firebaseFirestore
+        .collection('messages')
+        .doc(groupChatId)
+        .collection(groupChatId)
+        .orderBy('timestamp', descending: true)
+        .limit(limit)
+        .snapshots();
+  }
+
+  void sendMessage(String content, int type, String groupChatId,
+      String currentUserId, String peerId,
+      {String duration = ""}) {
+    DocumentReference documentReference = firebaseFirestore
+        .collection('messages')
+        .doc(groupChatId)
+        .collection(groupChatId)
+        .doc(DateTime.now().millisecondsSinceEpoch.toString());
+
+    MessageChat messageChat = MessageChat(
+        idFrom: currentUserId,
+        idTo: peerId,
+        timestamp: DateTime.now().millisecondsSinceEpoch.toString(),
+        content: content,
+        type: type,
+        duration: duration);
+
+    FirebaseFirestore.instance.runTransaction((transaction) async {
+      transaction.set(
+        documentReference,
+        messageChat.toJson(),
+      );
+    });
+  }
+}
+
+class TypeMessage {
+  static const text = 0;
+  // static const image = 1;
+  // static const sticker = 2;
+  // static const audio = 3;
+}
+
+class MessageChat {
+  String idFrom;
+  String idTo;
+  String timestamp;
+  String content;
+  int type;
+  String? duration;
+
+  MessageChat(
+      {required this.idFrom,
+      required this.idTo,
+      required this.timestamp,
+      required this.content,
+      required this.type,
+      this.duration});
+
+  Map<String, dynamic> toJson() {
+    return {
+      "idFrom": idFrom,
+      "idTo": idTo,
+      "timestamp": timestamp,
+      "content": content,
+      "type": type,
+      "duration": duration
+    };
+  }
+
+  factory MessageChat.fromDocument(DocumentSnapshot doc) {
+    String idFrom = doc.get('idFrom');
+    String idTo = doc.get('idTo');
+    String timestamp = doc.get('timestamp');
+    String content = doc.get('content');
+    int type = doc.get('type');
+    String duration = doc.get('duration');
+    return MessageChat(
+        idFrom: idFrom,
+        idTo: idTo,
+        duration: duration,
+        timestamp: timestamp,
+        content: content,
+        type: type);
+  }
+}
---
+++ lib/tips2/userList.dart
@@ -0,0 +1,213 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_30_tips/home.dart';
+import 'package:get/get.dart';
+
+import 'chat-details.dart';
+
+class UserList extends StatefulWidget {
+  const UserList({Key? key}) : super(key: key);
+
+  @override
+  _UserListState createState() => _UserListState();
+}
+
+class _UserListState extends State<UserList> {
+  Stream<List<QueryDocumentSnapshot<Object?>>> _stream() async* {
+    List<QueryDocumentSnapshot<Object?>> matchedData = [];
+
+    await FirebaseFirestore.instance.collection('chat').get().then((value) {
+      matchedData = value.docs.toList();
+
+      //return matchedData;
+    });
+
+    yield matchedData;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _stream();
+    //abc();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: customAppBar("Tips 2"),
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "UserList",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                child: StreamBuilder<List<QueryDocumentSnapshot<Object?>>>(
+                  stream: _stream(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                            //valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                            ),
+                      );
+                    } else {
+                      return snapshot.data!.isEmpty
+                          ? Center(
+                              child: Text("No Data"),
+                            )
+                          : ListView.builder(
+                              padding: EdgeInsets.all(10.0),
+                              itemBuilder: (context, index) {
+                                if (snapshot.data![index] == null) {
+                                  return SizedBox();
+                                } else {
+                                  // if (snapshot.data![index]["id"] == profileController.userC.id) {
+                                  //   return SizedBox.shrink();
+                                  // }
+                                  return GestureDetector(
+                                    onTap: () async {
+                                      await Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                          builder: (context) => ChatDetailPage(
+                                            data: snapshot.data![index],
+                                          ),
+                                        ),
+                                      );
+                                      // Get.to(ChatDetailPage(
+                                      //   data: snapshot.data![index],
+                                      // ));
+                                    },
+                                    child: Container(
+                                        color:
+                                            ThemeData().scaffoldBackgroundColor,
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Icon(
+                                                  Icons.account_circle,
+                                                  size: 50,
+                                                ),
+                                                SizedBox(
+                                                  width: 10,
+                                                ),
+                                                Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.start,
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: [
+                                                    Text(
+                                                      snapshot.data![index]
+                                                          ["name"],
+                                                      style: TextStyle(
+                                                          color: Colors.black,
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                          fontSize: 13),
+                                                    ),
+                                                    Text(
+                                                      snapshot.data![index].id
+                                                          .toString(),
+                                                      style: TextStyle(
+                                                          color: Colors.grey,
+                                                          fontSize: 12),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
+                                            Text(
+                                              "",
+                                              style:
+                                                  TextStyle(color: Colors.grey),
+                                            ),
+                                          ],
+                                        )),
+                                  );
+                                }
+                                ;
+                              },
+                              itemCount: snapshot.data!.length,
+                            );
+                    }
+                  },
+                ),
+              ),
+              // ListUserList(
+              //   text: '',
+              // ),
+            ],
+          ),
+        )));
+  }
+}
+
+class CircularUserImage extends StatefulWidget {
+  final String imageURL;
+  final double height, width;
+  const CircularUserImage(
+      {Key? key, required this.imageURL, this.height = 50.0, this.width = 50.0})
+      : super(key: key);
+
+  @override
+  State<CircularUserImage> createState() => _CircularUserImageState();
+}
+
+class _CircularUserImageState extends State<CircularUserImage> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(50),
+      child: Image.network(
+        widget.imageURL,
+        fit: BoxFit.cover,
+        width: widget.width,
+        height: widget.height,
+        loadingBuilder: (BuildContext context, Widget child,
+            ImageChunkEvent? loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            width: 50,
+            height: 50,
+            child: Center(
+              child: CircularProgressIndicator(
+                color: Colors.black,
+                value: loadingProgress.expectedTotalBytes != null
+                    ? loadingProgress.cumulativeBytesLoaded /
+                        loadingProgress.expectedTotalBytes!
+                    : null,
+              ),
+            ),
+          );
+        },
+        errorBuilder: (context, object, stackTrace) {
+          return Icon(
+            Icons.account_circle,
+            size: 50,
+            color: Colors.grey,
+          );
+        },
+      ),
+    );
+  }
+}
