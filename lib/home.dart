2c288a442b540c360e598c43bb80f09ed9904a84
Lakshy-1 <lvikramsah@gmail.com>
2022-08-28 13:50:43 +0545

    ui modified

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_30_tips/tips1.dart';
+import 'package:flutter_30_tips/tips2/userList.dart';
 import 'package:get/get.dart';
+
+final Color mainColor = Color(0xff8e412e);
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -17,21 +20,68 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Color(0xff8e412e),
-          centerTitle: true,
-          title: Text(
-            "Flutter 30 Tips",
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
-          )),
-      body: ListView(children: [
-        ListTile(
-          title: Text("Tips 1"),
-          tileColor: Color(0xff8e412e).withOpacity(0.2),
-          onTap: () => Get.to(SearchScreen()),
-        )
-      ]),
+      appBar:
+          customAppBar("Flutter 30 Tips\nwith\nLakshydeep Vikram", back: false),
+      body: ListView.separated(
+        itemCount: _list.length,
+        itemBuilder: (_, i) {
+          return tips(_list[i].label!, _list[i].title!, _list[i].go!);
+        },
+        separatorBuilder: (_, i) {
+          return Container(
+            width: double.infinity,
+            height: 2,
+            color: mainColor.withOpacity(0.1),
+          );
+        },
+      ),
     );
   }
+
+  final List<TIPS> _list = [
+    TIPS(label: "1", title: "Search In Flutter", go: SearchScreen()),
+    TIPS(label: "2", title: "Chatting with Firebase", go: UserList()),
+  ];
+  Widget tips(String label, String title, Widget go) {
+    return ListTile(
+      title: Text(
+        "Flutter Tips $label",
+        style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+      ),
+      trailing: Text(
+        "FT $label",
+        style: TextStyle(
+            fontWeight: FontWeight.w900,
+            fontSize: 28,
+            color: mainColor.withOpacity(0.35)),
+      ),
+      subtitle: Text(
+        title,
+        style: TextStyle(
+            fontSize: 25, fontWeight: FontWeight.w900, color: mainColor),
+      ),
+      tileColor: mainColor.withOpacity(0.2),
+      onTap: () => Get.to(go),
+    );
+  }
+}
+
+AppBar customAppBar(String title, {bool back = true}) {
+  return AppBar(
+      toolbarHeight: back ? 60 : 100,
+      backgroundColor: mainColor,
+      automaticallyImplyLeading: back,
+      centerTitle: true,
+      title: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
+      ));
+}
+
+class TIPS {
+  String? label, title;
+  Widget? go;
+  TIPS({this.go, this.label, this.title});
 }
